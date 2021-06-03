792264d3f59ad26ced7a52ff773a071a90f76dd4
averak <abe12@mccc.jp>
2021-06-04 02:28:33 +0900

    [test] GET/POST/PUT/DELETE test client

--- app/src/test/java/dev/abelab/crs/api/controller/AbstractRestControllerTest.java
+++ app/src/test/java/dev/abelab/crs/api/controller/AbstractRestControllerTest.java
@@ -1,6 +1,14 @@
 package dev.abelab.crs.api.controller;
 
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.util.MultiValueMap;
+
+import dev.abelab.crs.util.ConvertUtil;
+
 
 /**
  * Abstract Rest Controller Test
@@ -10,5 +18,89 @@ public abstract class AbstractRestControllerTest {
 
 	static final int SAMPLE_INT = 1;
 	static final String SAMPLE_STR = "SAMPLE";
+
+	/**
+	 * The Mock MVC
+	 */
+	MockMvc mockMvc;
+
+	/**
+	 * GET request
+	 *
+	 * @param path path
+	 *
+	 * @return HTTP request builder
+	 */
+	public MockHttpServletRequestBuilder getRequest(final String path) {
+		return MockMvcRequestBuilders.get(path);
+	}
+
+	/**
+	 * POST request
+	 *
+	 * @param path path
+	 *
+	 * @return HTTP request builder
+	 */
+	public MockHttpServletRequestBuilder postRequest(final String path) {
+		return MockMvcRequestBuilders.post(path);
+	}
+
+	/**
+	 * POST request (Form)
+	 *
+	 * @param path   path
+	 *
+	 * @param params query params
+	 *
+	 * @return HTTP request builder
+	 */
+	public MockHttpServletRequestBuilder postRequest(final String path,
+		final MultiValueMap<String, String> params) {
+		return MockMvcRequestBuilders.post(path) //
+			.contentType(MediaType.APPLICATION_FORM_URLENCODED) //
+			.params(params);
+	}
+
+	/**
+	 * POST request (JSON)
+	 *
+	 * @param path    path
+	 *
+	 * @param content request body
+	 *
+	 * @return HTTP request builder
+	 */
+	public MockHttpServletRequestBuilder postRequest(final String path, final Object content) {
+		return MockMvcRequestBuilders.post(path) //
+			.contentType(MediaType.APPLICATION_JSON) //
+			.content(ConvertUtil.convertObjectToJson(content));
+	}
+
+	/**
+	 * PUT request (JSON)
+	 *
+	 * @param path    path
+	 *
+	 * @param content request body
+	 *
+	 * @return HTTP request builder
+	 */
+	public MockHttpServletRequestBuilder putRequest(final String path, final Object content) {
+		return MockMvcRequestBuilders.put(path) //
+			.contentType(MediaType.APPLICATION_JSON) //
+			.content(ConvertUtil.convertObjectToJson(content));
+	}
+
+	/**
+	 * DELETE request
+	 *
+	 * @param path path
+	 *
+	 * @return HTTP request builder
+	 */
+	public MockHttpServletRequestBuilder deleteRequest(final String path) {
+		return MockMvcRequestBuilders.delete(path);
+	}
 
 }
