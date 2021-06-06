91abefc28cec4d7c3da0545576fbbc2eb093197c
averak <abe12@mccc.jp>
2021-06-06 21:09:36 +0900

    [fix] init DB before running test

---
+++ app/src/main/java/dev/abelab/crs/config/TestConfig.java
@@ -0,0 +1,19 @@
+package dev.abelab.crs.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
+
+/**
+ * Test Configuration
+ */
+@Configuration
+public class TestConfig {
+    @Bean
+    public FlywayMigrationStrategy clean() {
+        return flyway -> {
+            flyway.clean();
+            flyway.migrate();
+        };
+    }
+}
