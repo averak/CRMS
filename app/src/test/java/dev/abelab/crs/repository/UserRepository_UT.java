3fdec43bf05e6b1f010bf71f544177eebea5c698
averak <abe12@mccc.jp>
2021-06-11 04:39:07 +0900

    [refactor] devide class for each method

--- app/src/test/java/dev/abelab/crs/repository/UserRepository_UT.java
+++ app/src/test/java/dev/abelab/crs/repository/UserRepository_UT.java
@@ -2,9 +2,12 @@ package dev.abelab.crs.repository;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS;
 import static org.mockito.ArgumentMatchers.anyInt;
 
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import mockit.Expectations;
 import mockit.Injectable;
 import mockit.Tested;
@@ -30,54 +33,72 @@ class UserRepository_UT extends AbstractRepository_UT {
 
 	private final User user = UserSample.builder().build();
 
-	@Test
-	void 正_ユーザを保存する() {
-		new Expectations(this.userRepository) {
-			{
-				userRepository.insert(user);
-				result = user.getId();
-			}
-		};
+	/**
+	 * Test for insert user
+	 */
+	@Nested
+	@TestInstance(PER_CLASS)
+	class InsertUserTest {
 
-		assertThat(this.userRepository.insert(this.user)).isEqualTo(this.user.getId());
+		@Test
+		void 正_ユーザを保存する() {
+			new Expectations(userRepository) {
+				{
+					userRepository.insert(user);
+					result = user.getId();
+				}
+			};
+
+			assertThat(userRepository.insert(user)).isEqualTo(user.getId());
+		}
+
+		@Test
+		void 異_メールアドレスが既に登録済み() {
+			new Expectations(userRepository) {
+				{
+					userRepository.insert(user);
+					result = new ConflictException(ErrorCode.CONFLICT_EMAIL);
+				}
+			};
+
+			final var exception = assertThrows(BaseException.class, () -> userRepository.insert(user));
+			assertThat(exception.getErrorCode()).isEqualTo(ErrorCode.CONFLICT_EMAIL);
+		}
+
 	}
 
-	@Test
-	void 異_メールアドレスが既に登録済み() {
-		new Expectations(this.userRepository) {
-			{
-				userRepository.insert(user);
-				result = new ConflictException(ErrorCode.CONFLICT_EMAIL);
-			}
-		};
+	/**
+	 * Test for select user
+	 */
+	@Nested
+	@TestInstance(PER_CLASS)
+	class SelectUserTest {
 
-		final var exception = assertThrows(BaseException.class, () -> userRepository.insert(this.user));
-		assertThat(exception.getErrorCode()).isEqualTo(ErrorCode.CONFLICT_EMAIL);
-	}
+		@Test
+		void 正_ユーザが存在する() {
+			new Expectations(userRepository) {
+				{
+					userRepository.selectById(user.getId());
+					result = user;
+				}
+			};
 
-	@Test
-	void 正_ユーザが存在する() {
-		new Expectations(this.userRepository) {
-			{
-				userRepository.selectById(user.getId());
-				result = user;
-			}
-		};
+			assertThat(userRepository.selectById(user.getId())).isEqualTo(user);
+		}
 
-		assertThat(this.userRepository.selectById(this.user.getId())).isEqualTo(this.user);
-	}
+		@Test
+		void 異_ユーザが存在しない() {
+			new Expectations(userRepository) {
+				{
+					userRepository.selectById(anyInt);
+					result = new NotFoundException(ErrorCode.NOT_FOUND_USER);
+				}
+			};
 
-	@Test
-	void 異_ユーザが存在しない() {
-		new Expectations(this.userRepository) {
-			{
-				userRepository.selectById(anyInt);
-				result = new NotFoundException(ErrorCode.NOT_FOUND_USER);
-			}
-		};
+			final var exception = assertThrows(BaseException.class, () -> userRepository.selectById(anyInt()));
+			assertThat(exception.getErrorCode()).isEqualTo(ErrorCode.NOT_FOUND_USER);
+		}
 
-		final var exception = assertThrows(BaseException.class, () -> userRepository.selectById(anyInt()));
-		assertThat(exception.getErrorCode()).isEqualTo(ErrorCode.NOT_FOUND_USER);
 	}
 
 }
