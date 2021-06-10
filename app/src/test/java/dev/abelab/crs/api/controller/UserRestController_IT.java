3a60a25836f49dc8afd061210c77aa7ff6c9ffd9
averak <abe12@mccc.jp>
2021-06-11 03:53:32 +0900

    [test] create user api test

--- app/src/test/java/dev/abelab/crs/api/controller/UserRestController_IT.java
+++ app/src/test/java/dev/abelab/crs/api/controller/UserRestController_IT.java
@@ -3,8 +3,6 @@ package dev.abelab.crs.api.controller;
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.TestInstance.Lifecycle.*;
 
-import java.util.Date;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -12,7 +10,8 @@ import org.junit.jupiter.api.TestInstance;
 import org.springframework.http.HttpStatus;
 
 import dev.abelab.crs.db.entity.UserSample;
-import dev.abelab.crs.db.mapper.UserMapper;
+import dev.abelab.crs.repository.UserRepository;
+import dev.abelab.crs.enums.UserRoleEnum;
 import dev.abelab.crs.api.request.UserRequest;
 import dev.abelab.crs.api.response.UserResponse;
 import dev.abelab.crs.api.response.UsersResponse;
@@ -25,9 +24,10 @@ public class UserRestController_IT extends AbstractRestController_IT {
 	// API PATH
 	static final String BASE_PATH = "/api/users";
 	static final String GET_USERS_PATH = BASE_PATH;
+	static final String CREATE_USER_PATH = BASE_PATH;
 
 	@Autowired
-	UserMapper userMapper;
+	UserRepository userRepository;
 
 	/**
 	 * ユーザ一覧取得APIのテスト
@@ -40,8 +40,8 @@ public class UserRestController_IT extends AbstractRestController_IT {
 		void 正_管理者がユーザ一覧を取得() throws Exception {
 			final var user1 = UserSample.builder().id(1).build();
 			final var user2 = UserSample.builder().id(2).build();
-			userMapper.insert(user1);
-			userMapper.insert(user2);
+			userRepository.insert(user1);
+			userRepository.insert(user2);
 
 			// send request
 			final var request = getRequest(GET_USERS_PATH);
@@ -68,24 +68,22 @@ public class UserRestController_IT extends AbstractRestController_IT {
 
 		@Test
 		void 正_ユーザを作成() throws Exception {
-			final var user = UserSample.builder().id(1).build();
-			userMapper.insert(user);
+			// create request body
+			final var requestBody = UserRequest.builder() //
+				.firstName(SAMPLE_STR) //
+				.lastName(SAMPLE_STR) //
+				.password(SAMPLE_STR) //
+				.email(SAMPLE_STR) //
+				.roleId(UserRoleEnum.MEMBER.getId()) //
+				.build();
 
 			// send request
-			final var request = getRequest(GET_USERS_PATH);
-			final var response = execute(request, HttpStatus.OK, UsersResponse.class);
+			final var request = postRequest(CREATE_USER_PATH, requestBody);
+			execute(request, HttpStatus.CREATED);
 
 			// verify
-			// 作成されているか確認
-			assertThat(response.getUsers()) //
-				.extracting(UserResponse::getId) //
-				.containsExactly(user.getId());
-
-			// ロールを確認
-			assertThat(response.getUsers()) //
-				.extracting("roleId") //
-				.containsExactly(user.getRoleId());
-
+			final var createdUser = userRepository.selectByEmail(requestBody.getEmail());
+            assertThat(createdUser.isPresent()).isTrue();
 		}
 
 	}
