27720ade6cbb03b95eace675605ac309565e834d
averak <abe12@mccc.jp>
2021-06-16 04:45:29 +0900

    [feat] routing

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+// page components
+import { LoginComponent } from 'src/app/page/login/login.component';
+
+const routes: Routes = [{ path: 'login', component: LoginComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
