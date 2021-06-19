59054c87551c81aa949d8fd66dfaef9b65d5e1c9
averak <abe12@mccc.jp>
2021-06-19 17:38:55 +0900

    [feat] 静的リソースへのアクセスは認証不要にした

--- app/src/main/java/dev/abelab/crms/WebSecurityConfig.java
+++ app/src/main/java/dev/abelab/crms/WebSecurityConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -16,6 +18,20 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Bean
 	PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+
+	/**
+	 * 静的リソースへのアクセスを許可
+	 */
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/v2/api-docs", "/swagger-resources/**", "/swagger-ui/**", "/webjars/**");
+	}
+
+	@Override
+	protected void configure(HttpSecurity auth) throws Exception {
+		auth.authorizeRequests().antMatchers("/home").permitAll().antMatchers("/admin").hasRole("ADMIN").anyRequest().authenticated().and()
+			.formLogin().and().httpBasic();
 	}
 
 }
