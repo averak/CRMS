3cc5a69a8e9c25ff5a2f1d7ee2c81bd687424b23
averak <abe12@mccc.jp>
2021-06-28 18:48:00 +0900

    [feat] timezone setting

---
+++ app/src/main/java/dev/abelab/crms/config/TimeZoneConfig.java
@@ -0,0 +1,18 @@
+package dev.abelab.crms.config;
+
+import java.util.TimeZone;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TimeZoneConfig {
+
+    @Bean
+    public TimeZone timeZone() {
+        final var defaultTimeZone = TimeZone.getTimeZone("Asia/Tokyo");
+        TimeZone.setDefault(defaultTimeZone);
+        return defaultTimeZone;
+    }
+
+}
